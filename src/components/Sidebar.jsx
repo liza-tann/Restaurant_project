b98d506f4d8d88d92f01cdf73b0915fa6b4c1f7b
JungheangXD <127597359+JungheangXD@users.noreply.github.com>
2024-01-16 11:09:14 +0700

    Update Sidebar.jsx
    edit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,36 +9,36 @@ import { NavLink } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <div className="flex bg-light">
-      <div className="text-14 font-400 flex text-black p-3 pt-4">
+      <div className="flex text-black p-3 pt-4">
         <div>
           <ul className='pt-1'>
             <li className="mb-4">
-              <NavLink to="/Breakfast" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
-              <MdOutlineFreeBreakfast className='text-black w-6 h-6 mx-4'  />
+              <NavLink to="/Breakfast" className="hover:text-main w-40 h-40 ml-12">
+              <MdOutlineFreeBreakfast className='text-black opacity-70 w-8 h-8 mx-4'  />
                 Breakfast
               </NavLink>
             </li>
             <li className="mb-4">
-              <NavLink to="/lunch" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
-                <MdOutlineLunchDining className='text-black w-6 h-6 mx-4' />
+              <NavLink to="/lunch" className="hover:text-main w-40 h-40 ml-12">
+                <MdOutlineLunchDining className='text-black opacity-70 w-8 h-8 mx-4' />
                 <p className='mx-3'>lunch</p>
               </NavLink>
             </li>
             <li className="mb-4">
-              <NavLink to="/Dinner" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
-                <MdOutlineDinnerDining className='text-black w-6 h-6 mx-4' />
+              <NavLink to="/Dinner" className="hover:text-main w-40 h-40 ml-12">
+                <MdOutlineDinnerDining className='text-black opacity-70 w-8 h-8 mx-4' />
                 <p className='mx-3'>Dinner</p>
               </NavLink>
             </li>
             <li className="mb-4">
-              <NavLink to="/Dessert" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
-                <LuDessert className='text-black w-6 h-6 mx-4' />
+              <NavLink to="/Dessert" className="hover:text-main w-40 h-40 ml-12">
+                <LuDessert className='text-black opacity-70 w-8 h-8 mx-4' />
                 <p className='mx-2'>Dessert</p>
               </NavLink>
             </li>
             <li className="mb-4">
-              <NavLink to="/Beverages" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
-                <BiDrink className='text-black w-6 h-6 mx-4'  />
+              <NavLink to="/Beverages" className="hover:text-main w-40 h-40 ml-12">
+                <BiDrink className='text-black opacity-70 w-8 h-8 mx-4'  />
                 Beverages
               </NavLink>
             </li>
