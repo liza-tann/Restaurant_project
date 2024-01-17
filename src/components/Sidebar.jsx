36af838a827cf3ecbc4f500647ce62264753ce3d
JungheangXD <127597359+JungheangXD@users.noreply.github.com>
2024-01-17 09:18:52 +0700

    Update Sidebar.jsx
    Change NavLink tag to Link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { MdOutlineDinnerDining } from "react-icons/md";
 import { BiFoodMenu } from "react-icons/bi";
 import { LuDessert } from "react-icons/lu";
 import { BiDrink } from "react-icons/bi";
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
@@ -14,16 +14,16 @@ const Sidebar = () => {
         <div>
           <ul className='pt-1'>
             <li className="mb-4">
-              <NavLink to="/Breakfast" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
+              <Link to="/Breakfast" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
               <MdOutlineFreeBreakfast className='transition duration-300 ease-in-out transform hover:text-main hover:scale-110 text-black opacity-70 w-8 h-8 mx-4'  />
                 Breakfast
-              </NavLink>
+              </Link>
             </li>
             <li className="mb-4">
-              <NavLink to="/lunch" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
+              <Link to="/lunch" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
                 <MdOutlineLunchDining className='hover:text-main transition duration-300 ease-in-out transform hover:text-main hover:scale-110 text-black opacity-70 w-8 h-8 mx-4' />
                 <p className='mx-3'>lunch</p>
-              </NavLink>
+              </Link>
             </li>
             <li className="mb-4">
               <NavLink to="/Dinner" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
@@ -32,24 +32,24 @@ const Sidebar = () => {
               </NavLink>
             </li>
             <li className="mb-4">
-              <NavLink to="/Dessert" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
+              <Link to="/Dessert" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
                 <LuDessert className='hover:text-main transition duration-300 ease-in-out transform hover:text-main hover:scale-110 text-black opacity-70 w-8 h-8 mx-4' />
                 <p className='mx-2'>Dessert</p>
-              </NavLink>
+              </Link>
             </li>
             
             <li className="mb-4">
-              <NavLink to="/Beverages" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
+              <Link to="/Beverages" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
                 <BiDrink className='hover:text-main transition duration-300 ease-in-out transform hover:text-main hover:scale-110 text-black opacity-70 w-8 h-8 mx-4'  />
                 Beverages
-              </NavLink>
+              </Link>
             </li>
 
             <li className="mb-4">
-              <NavLink to="/" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
+              <Link to="/" className="hover:text-main hover:font-bold w-40 h-40 ml-12">
                 <BiFoodMenu className='hover:text-main transition duration-300 ease-in-out transform hover:text-main hover:scale-110 text-black opacity-70 w-8 h-8 mx-4' />
                 <p className='mx-1'>All Menu</p>
-              </NavLink>
+              </Link>
             </li>
           </ul>
         </div>
