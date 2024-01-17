5c76376e682311adb8840bd975f0995fa72576ac
JungheangXD <127597359+JungheangXD@users.noreply.github.com>
2024-01-17 09:04:35 +0700

    Update Breakfast.jsx

--- src/components/pages/Breakfast.jsx
+++ src/components/pages/Breakfast.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import Sidebar from '../Sidebar'
-import Nav from '../Nav'
-import Subcate from '../Subcate'
-const Breakfast = () => {
+
+import Breakfast from '../Breakfast'
+
+const Breakfasts = () => {
   return (
     <div>
-        <Nav />
-        <Sidebar />
-        <Subcate />
-      <p>Breakfast</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptatum commodi quod natus veniam? Quis quaerat molestias doloremque atque amet, natus nemo, dolores velit facere corrupti, tenetur asperiores autem officia!</p>
+        <Breakfast/>
     </div>
   )
 }
 
-export default Breakfast
+export default Breakfasts
